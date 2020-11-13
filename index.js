83e1252b9fc92f375d0b9f3697f8987fbb4974e1
adnankabir121 <adnankabir618@gmail.com>
2020-11-13 15:42:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+//Step 1: Identify Object Model
+/*
+    USER
+        -name
+        -id
+        -email
+        -password
+        -address
+*/
+
+// Step 2: Create Model URI/URL
+/*
+    http://example.com/api/users GET
+    http://example.com/api/users POST
+    http://example.com/api/users/:id PUT/PATCH
+    http://example.com/api/users/:id DELETE
+*/
+
+// Step 3: Determine Representation(Array of objects)
+/*
+[
+    {
+        "name":"Adnan Kabir Akon",
+        "id":0298,
+        "email":"adnankabir618@gmail.com",
+        "password":2941244,
+        "address":"park street,kalkata-1029"
+    },
+    {
+        "name":"Adnan Kabir Akon",
+        "id":0298,
+        "email":"adnankabir618@gmail.com",
+        "password":2941244,
+        "address":"park street,kalkata-1029"
+    },
+    {
+        "name":"Adnan Kabir Akon",
+        "id":0298,
+        "email":"adnankabir618@gmail.com",
+        "password":2941244,
+        "address":"park street,kalkata-1029"
+    }
+]
+*/
+//Single Data Representation
+/*
+    {
+        name:"Adnan Kabir Akon",
+        id:0298,
+        email:"adnankabir618@gmail.com",
+        password:2941244,
+        address:"park street,kalkata-1029"
+    }
+*/
+
+// Step 4: JSON View
+/*
+    If you hit this address : http://example.com/api/users
+    server will show all the information of users like below :
+
+    users:[
+        [
+            {
+                "name":"Adnan Kabir Akon",
+                "id":0298,
+                "email":"adnankabir618@gmail.com",
+                "password":2941244,
+                "address":"park street,kalkata-1029"
+            },
+            {
+                "name":"Adnan Kabir Akon",
+                "id":0298,
+                "email":"adnankabir618@gmail.com",
+                "password":2941244,
+                "address":"park street,kalkata-1029"
+            },
+            {
+                "name":"Adnan Kabir Akon",
+                "id":0298,
+                "email":"adnankabir618@gmail.com",
+                "password":2941244,
+                "address":"park street,kalkata-1029"
+            }
+        ]
+    ]
+*/
